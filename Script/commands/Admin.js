bbb78b3179956c2413f7092faed7282f4c7dfe2d
niloy78k <mentalniloy32@gmail.com>
2025-09-29 20:21:25 +0530

    Update Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -7,7 +7,7 @@ module.exports.config = {
  name: "admin",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦",
+ credits: "𝐏𝐨𝐥𝐚𝐛𝐚𝐳 𝐍𝐚𝐭𝐚𝐬𝐚",
  description: "Show Owner Info",
  commandCategory: "info",
  usages: "admin",
